09f637ccc8eb65da5ec35eeeec340debca57c4f2
UdulaPahasara <udulapahasara2@gmail.com>
2026-07-13 11:40:59 -0700

    feat: implement Projects page with alternating feature cards and scroll reveal animations

--- Sledaa/src/App.jsx
+++ Sledaa/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import AboutUs from './pages/AboutUs.jsx';
 import Event from './pages/Event.jsx';
 import News from './pages/News.jsx';
+import Projects from './pages/Projects.jsx';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/events" element={<Event />} />
         <Route path="/news" element={<News />} />
+        <Route path="/projects" element={<Projects />} />
         {/* You can add more routes here for other pages like /contact-us, etc. */}
       </Routes>
     </Router>
